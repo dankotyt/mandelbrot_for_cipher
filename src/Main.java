02510fac844ebc30a2bf4e60c12b3a4dc17bbb23
Dankotyt <danilkotlarov1@gmail.com>
2024-10-23 01:39:37 +0300

    add logic for encoder/decoder

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,101 @@
 import javax.swing.*;
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            // Loading image
-            BufferedImage image = ImageIO.read(new File("resources/input.jpg"));
-            int width = image.getWidth();
-            int height = image.getHeight();
-
-            // Translate image into matrix of pixels
-            Model_ImageMatrix myModel = new Model_ImageMatrix(image, height, width);
-            myModel.translatePixelsToNumbers(height, width);
-
-            // Show image and matrix
-            View_ImageMatrix myView = new View_ImageMatrix("Your image", image, myModel.getImageMatrix());
-            myView.showImage();
-            myView.setTitle("Your matrix");
-            myView.showMatrix();
-
-            BufferedImage image2 = ImageIO.read(new File("resources/mandelbrot1.png"));
-            int width2 = image2.getWidth();
-            int height2 = image2.getHeight();
-
-            // Translate image into matrix of pixels
-            Model_ImageMatrix myModel2 = new Model_ImageMatrix(image2, height2, width2);
-            myModel2.translatePixelsToNumbers(height2, width2);
-
-            // Show image and matrix
-            View_ImageMatrix myView2 = new View_ImageMatrix("Your image", image2, myModel2.getImageMatrix());
-            myView2.showImage();
-            myView2.setTitle("Your matrix");
-            myView2.showMatrix();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Mandelbrot");
-            Mandelbrot mandelbrot = new Mandelbrot();
-            frame.add(mandelbrot);
+            MandelbrotView view = new MandelbrotView();
+            MandelbrotModel model = new MandelbrotModel();
+            MandelbrotController controller = new MandelbrotController(model, view);
+
+            frame.add(view);
             frame.setSize(1024, 720);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setVisible(true);
 
-            mandelbrot.generateImage();
+            controller.generateImage();
+            controller.showImageMatrix();
         });
     }
+//main для шифратора и дешифратора, когда будет работать
+//    public static void main(String[] args) {
+//        try {
+//            // Loading image
+//            BufferedImage originalImage = ImageIO.read(new File("resources/input.jpg"));
+//            int width = originalImage.getWidth();
+//            int height = originalImage.getHeight();
+//
+//            // Изменение размеров исходного изображения, если они не совпадают с требуемыми
+//            if (originalImage.getWidth() != width || originalImage.getHeight() != height) {
+//                originalImage = Model.resizeImage(originalImage, width, height);
+//            }
+//
+//            // Translate image into matrix of pixels
+//            Model originalModel = new Model(originalImage, height, width);
+//            originalModel.translatePixelsToNumbers(height, width);
+//
+//            // Show image and matrix
+//            View originalView = new View("Original image", originalImage, originalModel.getImageMatrix());
+//            originalView.showImage();
+//            originalView.setTitle("Original matrix");
+//            originalView.showMatrix();
+//
+//            BufferedImage mandelbrotImage = ImageIO.read(new File("resources/mandelbrot1.png"));
+//            int mandelbrotImageWidth = mandelbrotImage.getWidth();
+//            int mandelbrotImageHeight = mandelbrotImage.getHeight();
+//
+//            // Изменение размеров изображения множества Мандельброта, если они не совпадают с требуемыми
+//            if (mandelbrotImageWidth != width || mandelbrotImageHeight != height) {
+//                mandelbrotImage = Model.resizeImage(mandelbrotImage, width, height);
+//                mandelbrotImageWidth = width;
+//                mandelbrotImageHeight = height;
+//            }
+//            // Translate image into matrix of pixels
+//            Model mandelbrotModel = new Model(mandelbrotImage, mandelbrotImageHeight, mandelbrotImageWidth);
+//            mandelbrotModel.translatePixelsToNumbers(mandelbrotImageHeight, mandelbrotImageWidth);
+//
+//            // Show image and matrix
+//            View mandelbrotView = new View("Mandelbrot image", mandelbrotImage, mandelbrotModel.getImageMatrix());
+//            mandelbrotView.showImage();
+//            mandelbrotView.setTitle("Mandelbrot matrix");
+//            mandelbrotView.showMatrix();
+//
+//            // Шифрование исходного изображения
+//            double[][] encryptedMatrix = originalModel.encryptImage(mandelbrotModel.getImageMatrix());
+//            BufferedImage encryptedImage = originalModel.matrixToImage(encryptedMatrix, width, height);
+//
+//            // Отображение зашифрованного изображения
+//            View encryptedView = new View("Encrypted Image", encryptedImage, encryptedMatrix);
+//            encryptedView.showImage();
+//
+//            // Запрос пользователю о расшифровке изображения
+//            int option = JOptionPane.showConfirmDialog(null, "Хотите расшифровать изображение?", "Расшифровать изображение", JOptionPane.YES_NO_OPTION);
+//            if (option == JOptionPane.YES_OPTION) {
+//                // Дешифрование изображения
+//                double[][] decryptedMatrix = originalModel.decryptImage(encryptedMatrix, mandelbrotModel.getImageMatrix());
+//                BufferedImage decryptedImage = originalModel.matrixToImage(decryptedMatrix, width, height);
+//
+//                // Отображение расшифрованного изображения
+//                View decryptedView = new View("Decrypted Image", decryptedImage, decryptedMatrix);
+//                decryptedView.showImage();
+//            } else {
+//                // Изображение остается зашифрованным
+//                JOptionPane.showMessageDialog(null, "Изображение осталось зашифрованным.");
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        SwingUtilities.invokeLater(() -> {
+//            JFrame frame = new JFrame("Mandelbrot");
+//            Mandelbrot mandelbrot = new Mandelbrot();
+//            frame.add(mandelbrot);
+//            frame.setSize(1024, 720);
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            frame.setVisible(true);
+//
+//            mandelbrot.generateImage();
+//        });
+//    }
 }
--- src/Model_ImageMatrix.java
+++ src/Model_ImageMatrix.java
@@ -1,5 +1,6 @@
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.Graphics2D;
 
 /**
  * @author Boykov Iliya
@@ -62,5 +63,106 @@ public class Model_ImageMatrix {
                 imageMatrix[i][j] = pixels[i * my_width + j] & 0xFF; // Translate byte to int
             }
         }
+    }
+
+    /**
+     * Изменяет размер изображения до нужных размеров.
+     *
+     * @param image Изображение для изменения размера.
+     * @param width Новая ширина.
+     * @param height Новая высота.
+     * @return Изображение с измененными размерами.
+     *
+     * @author andrey
+     */
+    public static BufferedImage resizeImage(BufferedImage image, int width, int height) {
+        BufferedImage resizedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = resizedImage.createGraphics();
+        g2d.drawImage(image, 0, 0, width, height, null);
+        g2d.dispose();
+        return resizedImage;
+    }
+
+    /**
+     * Шифрует изображение с использованием матрицы множества Мандельброта через операцию XOR.
+     *
+     * @param mandelbrotMatrix Матрица множества Мандельброта.
+     * @return Зашифрованная матрица пикселей.
+     * @author andrey
+     */
+    public double[][] encryptImage(double[][] mandelbrotMatrix) {
+        int height = imageMatrix.length;
+        int width = imageMatrix[0].length;
+        int mandelbrotHeight = mandelbrotMatrix.length;
+        int mandelbrotWidth = mandelbrotMatrix[0].length;
+
+        // Проверка размеров матриц
+        if (height != mandelbrotHeight || width != mandelbrotWidth) {
+            throw new IllegalArgumentException("Размеры матриц не совпадают");
+        }
+
+        double[][] encryptedMatrix = new double[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                encryptedMatrix[i][j] = (int) imageMatrix[i][j] ^ (int) mandelbrotMatrix[i][j];
+            }
+        }
+
+        return encryptedMatrix;
+    }
+
+    /**
+     * Дешифрует изображение с использованием матрицы множества Мандельброта через операцию XOR.
+     *
+     * @param encryptedMatrix Зашифрованная матрица пикселей.
+     * @param mandelbrotMatrix Матрица множества Мандельброта.
+     * @return Дешифрованная матрица пикселей.
+     *
+     * @author andrey
+     */
+    public double[][] decryptImage(double[][] encryptedMatrix, double[][] mandelbrotMatrix) {
+        int height = encryptedMatrix.length;
+        int width = encryptedMatrix[0].length;
+        int mandelbrotHeight = mandelbrotMatrix.length;
+        int mandelbrotWidth = mandelbrotMatrix[0].length;
+
+        // Проверка размеров матриц
+        if (height != mandelbrotHeight || width != mandelbrotWidth) {
+            throw new IllegalArgumentException("Размеры матриц не совпадают");
+        }
+
+        double[][] decryptedMatrix = new double[height][width];
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                decryptedMatrix[i][j] = (int) encryptedMatrix[i][j] ^ (int) mandelbrotMatrix[i][j];
+            }
+        }
+
+        return decryptedMatrix;
+    }
+
+    /**
+     * Преобразует матрицу пикселей в изображение.
+     *
+     * @param matrix Матрица пикселей.
+     * @param width Ширина изображения.
+     * @param height Высота изображения.
+     * @return Изображение.
+     *
+     * @author andrey
+     */
+    public BufferedImage matrixToImage(double[][] matrix, int width, int height) {
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                pixels[i * width + j] = (byte) matrix[i][j];
+            }
+        }
+
+        return image;
     }
 }
